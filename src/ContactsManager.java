ce2ce5ca56e566c12a7970f2ab3f5c594c36f235
Daryl Gossan <gossandaryl01@gmail.com>
2025-05-14 17:30:04 +0000

    Programme de Contact

---
+++ src/ContactsManager.java
@@ -0,0 +1,24 @@
+public class ContactsManager {
+        Contact[] contacts;
+        int count;
+
+        public ContactsManager() {
+            contacts = new Contact[100];
+            count = 0;
+        }
+
+        public void addContact(Contact contact) {
+            contacts[count] = contact;
+            count++;
+        }
+
+        public Contact searchContact(String name) {
+            for (int i = 0; i < count; i++) {
+                if (contacts[i].name.equalsIgnoreCase(name)) {
+                    return contacts[i];
+                }
+            }
+            return null;
+        }
+    }
+
